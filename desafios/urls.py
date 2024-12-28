cc79694b017736bcfe700318c4f67f5cb837fdfd
miguel977 <miguel645@gmail.com>
2024-12-28 08:23:56 -0300

    Criando mais rotas

--- desafios/urls.py
+++ desafios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import domingo
+from .views import domingo, segunda, terca
 
 urlpatterns = [
     path('domingo', domingo),
+    path('segunda', segunda),
+    path('terça', terca),
 ]
--- desafios/views.py
+++ desafios/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def domingo(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Ler Livro sobre Django")
+
+def segunda(request):
+    return HttpResponse("Assistir Série Breaking Bad")
+
+def terca(request):
+    return HttpResponse("Estudar programação Python")
 
 
 
