61f93a3f1c94e99cf941f75a60c62fb5b6e9ba1e
miguel977 <miguel645@gmail.com>
2025-01-01 09:49:13 -0300

    Atualizando model

--- models/movies/models.py
+++ models/movies/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Movie(models.Model):
     title = models.CharField(max_length=60)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    director = models.CharField(null=True, max_length=100)
+    winner_oscar = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.title} - {self.rating}"
+
