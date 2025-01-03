67e203972311187a980bd93d5a38e9fec417b14c
miguel977 <miguel645@gmail.com>
2025-01-03 10:16:05 -0300

    Recuperando dados no html

--- fundamentos/setup/views.py
+++ fundamentos/setup/views.py
@@ -1,11 +1,14 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 
 def homepage(request):
-    # return HttpResponse('Hello world')
-    return render(request, 'home.html')
-
+    nome = 'Fulano'
+    pessoa = {
+        'nome': 'Maria',
+        'idade': 30,
+        'cidade':'São Paulo'
+    }
+    return render(request, 'home.html', {'nome': nome, 'pessoa':pessoa})
 
 def about(request):
-    # return HttpResponse("about me")
-    return render(request, 'about.html')
+    frutas = ['Maçã', 'Banana', 'Laranja', 'Uva']
+    return render(request, 'about.html', {'frutas':frutas})
